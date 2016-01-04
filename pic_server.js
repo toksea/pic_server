ba07e2ae7104d903c20f34b43e4052974028239f
Xiaopei Li <toksea@gmail.com>
2016-01-04 20:18:42 +0800

    YAC :notebook_with_decorative_cover:

--- pic_server.js
+++ pic_server.js
@@ -3,8 +3,12 @@ if (Meteor.isClient) {
     Session.setDefault('tip', 'drag and drop here');
 
     Uploader.finished = function(index, fileInfo, templateContext) {
-        var tip = '已上传：' +
-            '![](' + fileInfo.url + ')';
+       console.log(fileInfo);
+
+        var tip = '已上传：'
+            // '![](' + fileInfo.url + ')';
+            + '<input size="60" value="[图片](' + 'http://pic.example.com/upload' + fileInfo.path + ')'
+           + '"/>';
 
         Session.set('tip', tip);
     };
@@ -27,6 +31,11 @@ if (Meteor.isServer) {
             tmpDir: '/tmp/pic_server/uploads',
             uploadDir: process.env.PWD + '/.uploads/',
             checkCreateDirectories: true,
+           maxFileSize: 11000000,
+           getFileName: function(fileInfo, formData) {
+               return 'Saved-' + Math.random().toString(36).substring(7)
+                       + fileInfo.name.substr(fileInfo.name.indexOf('.'));
+           },
             finished: function(fileInfo, formFields) {
                 // name String  File name
                 // size Number  Size in Bytes
